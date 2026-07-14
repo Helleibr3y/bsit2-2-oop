3810833df01b4274a199bfe4bfa000951a8292dc
Helleibrey <yecyech47275@liceo.edu.ph>
2026-07-14 12:01:46 +0800

    lab activity 3

---
+++ src/GradeTracker.java
@@ -0,0 +1,18 @@
+public class GradeTracker{
+
+    static double[] cutoffs = {90, 80, 70, 60};
+    static char[] letters = {'A','B','C','D',};
+
+    static char letterFor(double grade) {
+        for (int i = 0; i < cutoffs.length; i++) {
+            if (grade >= cutoffs[i])  return letters[i];
+        }
+        return 'F';
+    }
+    public static void main(String[] args) {
+        System.out.println(letterFor(95));
+        System.out.println(letterFor(72));
+        System.out.println(letterFor(50));
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    String name;
+    double grade;
+
+    Student(String name, double grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+}
